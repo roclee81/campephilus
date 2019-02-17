8671be48ac25765d3df2b114f87dec75b200f525
慢慢跑 <vinicolor.violet.end@gmail.com>
2019-02-17 14:18:44 +0800

    完整代码

---
+++ src/main/java/org/cqu/edu/mrc/annihilation/common/enums/ResponseEnum.java
@@ -0,0 +1,145 @@
+package org.cqu.edu.mrc.annihilation.common.enums;
+
+import lombok.Getter;
+
+/**
+ * realdata
+ * 服务器对请求进行回复的代码
+ * 但实际上通讯协议只是对请求码回复+1
+ *
+ * @author lx
+ * @version V1.0
+ * @date 2018/12/29 15:05
+ * @email vinicolor.violet.end@gmail.com
+ * Description:
+ */
+@Getter
+public enum ResponseEnum {
+
+    /**
+     * 未知错误
+     */
+    UNKNOWN_ERROR(-1),
+
+    /**
+     * 数据格式错误
+     */
+    DATA_FORMAT_ERROR(-2),
+
+    /**
+     * 请求的CODE是错误的
+     */
+    CODE_ERROR(-3),
+
+    /**
+     * 数据不存在
+     */
+    DATA_DOES_NOT_EXIST(-4),
+
+    /**
+     * 请求参数不存在
+     */
+    REQUEST_PARAMETER_DOES_NOT_EXIST(-5),
+
+    /**
+     * 成功
+     */
+    SUCCESS(200),
+
+    /**
+     * 开机注册成功
+     */
+    REGISTER_SUCCESS(100),
+
+    /**
+     * 服务器准备完毕，可以开始手术了
+     */
+    OPERATION_READY_SUCCESS(152),
+
+    /**
+     * 可以准备开始发送手术设备信息
+     */
+    OPERATION_DEVICE_READY_SUCCESS(154),
+
+    /**
+     * 该条信息保存完毕
+     */
+    OPERATION_DEVICE_SUCCESS(156),
+
+    /**
+     * 回复设备数据发送可以完毕
+     */
+    OPERATION_DEVICE_END_SUCCESS(158),
+
+    /**
+     * 可以开始上传病人数据
+     */
+    PATIENT_INFO_READY_SUCCESS(160),
+
+    /**
+     * 该条病人数据保存完毕
+     */
+    PATIENT_INFO_SUCCESS(162),
+
+    /**
+     * 病人信息发送完毕收到
+     */
+    PATIENT_INFO_END_SUCCESS(164),
+
+    /**
+     * 可以开始上传仪器数据
+     */
+    DEVICE_DATA_READY_SUCCESS(166),
+
+    /**
+     * 该条仪器数据保存完毕
+     */
+    DEVICE_DATA_SUCCESS(168),
+
+    /**
+     * 病人信息发送完毕收到
+     */
+    DEVICE_DATA_END_SUCCESS(170),
+
+    /**
+     * 手术过程中标记信息可以发送
+     */
+    OPERATION_MARK_READY_SUCCESS(172),
+
+    /**
+     * 该条过程中的标记数据收到
+     */
+    OPERATION_MARK_SUCCESS(174),
+
+    /**
+     * 手术过程中的标记数据发送结束收到
+     */
+    OPERATION_MARK_END_SUCCESS(176),
+
+    /**
+     * 是手术结束的信息
+     */
+    OPERATION_END_READY_SUCCESS(178),
+
+    /**
+     * 回复收到手术结束的信息
+     */
+    OPERATION_END_SUCCESS(180),
+
+    /**
+     * 可以发送术后患者信息，服务器已经准备好了
+     */
+    POSTOPERATIVE_PATIENT_INFO_READY_SUCCESS(182),
+
+    /**
+     * 收到术后患者信息
+     */
+    POSTOPERATIVE_PATIENT_INFO_SUCCESS(184),
+    ;
+
+    private Integer code;
+
+    ResponseEnum(Integer code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/org/cqu/edu/mrc/annihilation/common/utils/TimeStampUtil.java
@@ -0,0 +1,100 @@
+package org.cqu.edu.mrc.annihilation.common.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author Vinicolor
+ * @date 2018/8/29
+ * <p>
+ * Description:
+ * 时间戳工具类
+ */
+public class TimeStampUtil {
+
+    /**
+     * 得到当前的时间戳
+     *
+     * @return 当前时间戳
+     */
+    public static Integer getCurrentTimeStamp() {
+        return (int) (System.currentTimeMillis() / 1000);
+    }
+
+    /**
+     * 得到与当前时间前一天的时间戳
+     *
+     * @return 当前时间前一天的时间戳
+     */
+    public static Integer getDayBeforeTimeStamp() {
+        return getCurrentTimeStamp() - 86400;
+    }
+
+    /**
+     * @return
+     */
+    public static Date getDayBeforeDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        return calendar.getTime();
+    }
+
+    /**
+     * @return
+     */
+    public static Date getDayAfterDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, +1);
+        return calendar.getTime();
+    }
+
+    /**
+     * @return
+     */
+    public static Date getWeekBeforeDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -7);
+        return calendar.getTime();
+    }
+
+    /**
+     * 通过 year month date 得到Date实体，注意这里是返回的月份已经对月份减去了1，可以直接输入需要的月份
+     *
+     * @param year
+     * @param month
+     * @param date
+     * @return
+     */
+    public static Date getDate(Integer year, Integer month, Integer date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, date, 0, 0, 0);
+        return calendar.getTime();
+    }
+
+    /**
+     * 得到当前的时间
+     *
+     * @return
+     */
+    public static Date getDate() {
+        Calendar calendar = Calendar.getInstance();
+        return calendar.getTime();
+    }
+
+    /**
+     * 通过 year month date hourOfDay minute second得到Date实体，注意这里是返回的月份已经对月份减去了1，可以直接输入需要的月份
+     *
+     * @param year
+     * @param month
+     * @param date
+     * @param hourOfDay
+     * @param minute
+     * @param second
+     * @return
+     */
+    public static Date getDate(Integer year, Integer month, Integer date, Integer hourOfDay, Integer minute, Integer second) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, date, hourOfDay, minute, second);
+        return calendar.getTime();
+    }
+}
