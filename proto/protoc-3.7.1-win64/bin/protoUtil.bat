3af712ddf2ed36ea528cad706f9eb49eb0e8d363
慢慢跑 <vinicolor.violet.end@gmail.com>
2019-05-25 16:26:19 +0800

    1.上传生成协议文件

---
+++ proto/protoc-3.7.1-win64/bin/protoUtil.bat
@@ -0,0 +1,8 @@
+protoc --java_out=. ./protobuf_after_operation_info.proto
+protoc --java_out=. ./protobuf_before_operation_info.proto
+protoc --java_out=. ./protobuf_device_info.proto
+protoc --java_out=. ./protobuf_device_hospital_relation_info.proto
+protoc --java_out=. ./protobuf_operation_info.proto
+protoc --java_out=. ./protobuf_operation_mark_info.proto
+protoc --java_out=. ./protobuf_patient_info.proto
+
