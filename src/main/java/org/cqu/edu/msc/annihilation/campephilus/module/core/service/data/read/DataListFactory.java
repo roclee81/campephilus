6c216f86f7db8c668a77ef804f11c9a7decbedc3
慢慢跑 <vinicolor.violet.end@gmail.com>
2019-09-13 16:01:10 +0800

    1.更改Factory

--- src/main/java/org/cqu/edu/msc/annihilation/campephilus/module/core/service/data/read/DataListFactory.java
+++ src/main/java/org/cqu/edu/msc/annihilation/campephilus/module/core/service/data/read/DataListFactory.java
@@ -3,6 +3,8 @@ package org.cqu.edu.msc.annihilation.campephilus.module.core.service.data.read;
 import org.cqu.edu.msc.annihilation.campephilus.module.core.service.data.read.service.DataListService;
 import org.cqu.edu.msc.annihilation.campephilus.module.core.service.data.read.service.impl.Norwamd9002sDataListServiceImpl;
 import org.cqu.edu.msc.annihilation.campephilus.module.core.service.data.read.service.impl.PearlcareYy106DataListServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
@@ -13,7 +15,22 @@ import java.util.HashMap;
  * @email vinicolor.violet.end@gmail.com
  * Description:
  */
+@Component
 public class DataListFactory {
+
+    @Autowired
+    private Norwamd9002sDataListServiceImpl norwamd9002sDataListService;
+
+    @Autowired
+    private PearlcareYy106DataListServiceImpl pearlcareYy106DataListService;
+    
+    HashMap<String, DataListService> dataListServiceMap;
+
+
+    public DataListFactory(Norwamd9002sDataListServiceImpl norwamd9002sDataListService, PearlcareYy106DataListServiceImpl pearlcareYy106DataListService) {
+        this.norwamd9002sDataListService = norwamd9002sDataListService;
+        this.pearlcareYy106DataListService = pearlcareYy106DataListService;
+    }
 
     private static HashMap<String, DataListService> dataListServiceMap = new HashMap<>();
 
